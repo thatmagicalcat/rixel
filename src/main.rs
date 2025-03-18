056b52c2f0f94ad00a0583596323f582471b7731
thatmagicalcat <thatmagicalcat@hotmail.com>
2025-03-18 14:33:25 +0530

    basic ui layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,153 @@
+use piston::*;
+
+use graphics::Transformed;
+use opengl_graphics::GlGraphics;
+use opengl_graphics::GlyphCache;
+use opengl_graphics::Texture;
+use opengl_graphics::TextureSettings;
+
+use glfw_window::GlfwWindow;
+use glfw_window::OpenGL;
+
+use image::ImageBuffer;
+use image::Rgba;
+use image::RgbaImage;
+
+mod ui;
+
+const WINDOW_WIDTH: u32 = 1400;
+const WINDOW_HEIGHT: u32 = 900;
+
+const IMAGE_LEN: u32 = 80;
+const IMAGE_VIEWPORT_LEN: f64 = WINDOW_HEIGHT as f64 - 20.0;
+const SCALE: f64 = IMAGE_VIEWPORT_LEN / IMAGE_LEN as f64;
+
+pub type Color = [f32; 4];
+
+macro_rules! colors {
+    [ $($name:ident => [$r:expr, $g:expr, $b:expr]),* $(,)? ] => {
+        #[allow(unused)]
+        mod color {$(
+            pub const $name: super::Color = [$r, $g, $b, 1.0];
+        )*}
+    };
+}
+
+colors! {
+    RED => [1.0, 0.0, 0.0],
+    GREEN => [0.0, 1.0, 0.0],
+    BLUE => [0.0, 0.0, 1.0],
+    WHITE => [1.0, 1.0, 1.0],
+    GRAY => [0.5, 0.5, 0.5],
+    BLACK => [0.0, 0.0, 0.0],
+}
+
+struct App<'a> {
+    gl: GlGraphics,
+    glyph_cache: GlyphCache<'a>,
+    image_buffer: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    texture: Texture,
+    buttons: Vec<ui::UIButton>,
+}
+
+impl<'a> App<'a> {
+    fn new(opengl: OpenGL, mut glyph_cache: GlyphCache<'a>) -> Self {
+        let mut image_buffer = RgbaImage::new(IMAGE_LEN, IMAGE_LEN);
+        image_buffer
+            .get_mut(..)
+            .unwrap()
+            .iter_mut()
+            .for_each(|i| *i = 127);
+
+        let texture = Texture::from_image(
+            &image_buffer,
+            &TextureSettings::new().filter(opengl_graphics::Filter::Nearest),
+        );
+
+        Self {
+            buttons: vec![ui::UIButton::new(
+                "button with automatic dimension calculation",
+                color::WHITE,
+                color::RED,
+                17,
+                [910.0, 10.0],
+                [10.0, 10.0],
+                &mut glyph_cache,
+            )],
+            gl: GlGraphics::new(opengl),
+
+            glyph_cache,
+            image_buffer,
+            texture,
+        }
+    }
+
+    fn render(&mut self, args: &RenderArgs) {
+        self.gl.draw(args.viewport(), |c, gl| {
+            graphics::image(
+                &self.texture,
+                c.transform.trans(10.0, 10.0).scale(SCALE, SCALE),
+                gl,
+            );
+
+            graphics::rectangle(
+                color::WHITE,
+                graphics::rectangle::rectangle_by_corners(
+                    WINDOW_HEIGHT as _,
+                    10.0,
+                    WINDOW_HEIGHT as f64 + 2.0,
+                    WINDOW_HEIGHT as f64 - 10.0,
+                ),
+                c.transform,
+                gl,
+            );
+
+            for btn in &self.buttons {
+                btn.draw(&c, gl, &mut self.glyph_cache);
+            }
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {}
+
+    fn mouse_pos(&mut self, [x, y]: [f64; 2]) {
+        for b in &mut self.buttons {
+            b.color = if b.is_over(x, y) {
+                color::GRAY
+            } else {
+                color::WHITE
+            };
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_3;
+    let mut window: GlfwWindow = WindowSettings::new("Rix3l", [WINDOW_WIDTH, WINDOW_HEIGHT])
+        .graphics_api(opengl)
+        .resizable(false)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    gl::load_with(|s| window.window.get_proc_address(s));
+
+    let font_bytes = include_bytes!("../JetBrainsFont.ttf");
+    let mut app = App::new(
+        opengl,
+        GlyphCache::from_bytes(font_bytes, (), TextureSettings::new()).unwrap(),
+    );
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            app.render(&args);
+        }
+
+        if let Some(args) = e.update_args() {
+            app.update(&args);
+        }
+
+        e.mouse_cursor(|pos| app.mouse_pos(pos));
+    }
 }
---
+++ src/ui/mod.rs
@@ -0,0 +1,119 @@
+use crate::*;
+
+use graphics::CharacterCache;
+
+pub struct UIButton {
+    pub text: String,
+    pub color: Color,
+    pub text_color: Color,
+    pub size: u32,
+    pub pos: [f64; 2],
+    pub width: f64,
+    pub height: f64,
+    pub padding: [f64; 2],
+}
+
+impl UIButton {
+    pub fn new(
+        text: &str,
+        color: Color,
+        text_color: Color,
+        size: u32,
+        pos: [f64; 2],
+        padding: [f64; 2],
+        glyphs: &mut GlyphCache,
+    ) -> Self {
+        let (width, height) = measure_text_dimensions(text, size, glyphs);
+
+        UIButton {
+            text: text.to_string(),
+            color,
+            text_color,
+            size,
+            pos,
+            width,
+            height,
+            padding,
+        }
+    }
+
+    pub fn draw(
+        &self,
+        ctx: &graphics::Context,
+        graphics: &mut GlGraphics,
+        glyphs: &mut GlyphCache,
+    ) {
+        // draw button also according to padding
+        graphics::rectangle(
+            self.color,
+            [
+                self.pos[0],
+                self.pos[1],
+                self.width + self.padding[0] * 2.0,
+                self.height + self.padding[1] * 2.0,
+            ],
+            ctx.transform,
+            graphics,
+        );
+
+        // graphics::rectangle(
+        //     self.color,
+        //     [self.pos[0], self.pos[1], self.width, self.height],
+        //     ctx.transform,
+        //     graphics,
+        // );
+
+        draw_text(
+            ctx,
+            graphics,
+            glyphs,
+            self.text_color,
+            [
+                self.pos[0] + self.padding[0],
+                self.pos[1] + self.height - 1.0 + self.padding[1],
+            ],
+            &self.text,
+            self.size,
+        );
+    }
+
+    pub fn is_over(&self, x: f64, y: f64) -> bool {
+        x > self.pos[0]
+            && x < self.pos[0] + self.width
+            && y > self.pos[1]
+            && y < self.pos[1] + self.height
+    }
+}
+pub fn draw_text(
+    ctx: &graphics::Context,
+    graphics: &mut GlGraphics,
+    glyphs: &mut GlyphCache,
+    color: [f32; 4],
+    pos: [f64; 2],
+    text: &str,
+    font_size: u32,
+) {
+    graphics::Text::new_color(color, font_size)
+        .draw(
+            text,
+            glyphs,
+            &ctx.draw_state,
+            ctx.transform.trans(pos[0], pos[1]),
+            graphics,
+        )
+        .unwrap();
+}
+
+pub fn measure_text_dimensions(text: &str, font_size: u32, glyphs: &mut GlyphCache) -> (f64, f64) {
+    let mut w: f64 = 0.0;
+    let mut h: f64 = 0.0;
+
+    for ch in text.chars() {
+        let character = glyphs.character(font_size, ch).unwrap();
+        let top = character.top();
+        w += character.advance_width();
+        h = h.max(top);
+    }
+
+    (w, h)
+}
